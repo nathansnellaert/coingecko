2472bffb5eb544b33c797d47fcfc30798d789451
salmiakdrop <nathansnellaert@gmail.com>
2025-09-06 15:06:44 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+os.environ['CONNECTOR_NAME'] = 'coingecko'
+os.environ['RUN_ID'] = os.getenv('RUN_ID', 'local-run')
+
+from utils import validate_environment, upload_data
+from assets.coins.coins import process_coins
+from assets.prices.prices import process_prices
+
+def main():
+    validate_environment()
+    
+    coins_data = process_coins()
+    prices_data = process_prices(coins_data)
+    
+    upload_data(coins_data, "coins")
+    upload_data(prices_data, "prices")
+
+if __name__ == "__main__":
+    main()
